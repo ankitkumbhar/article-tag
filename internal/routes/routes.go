3f1160994e08e96c518bf8aa70c8db722fb5f102
ankitkumbhar <kumbharankit23@gmail.com>
2023-08-21 20:31:01 +0530

    updated popular tags endpoint

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -12,7 +12,7 @@ func InitRouter(app *handler.Application) *chi.Mux {
 	r.Post("/tags/{publication}", app.Store())
 	r.Get("/tags/{publication}", app.Get())
 	r.Delete("/tags/{publication}", app.Delete())
-	r.Get("/tags/popular/{publication}", app.PopularTag())
+	r.Get("/tags/{publication}/popular", app.PopularTag())
 
 	return r
 }
