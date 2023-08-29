10e3c1db20892c63bc53405da44094cb7edc5270
ankitkumbhar <kumbharankit23@gmail.com>
2023-08-29 17:51:55 +0530

    added not found and method not allowed custom methods

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,12 +2,24 @@ package routes
 
 import (
 	"article-tag/internal/handler"
+	"article-tag/internal/response"
+	"net/http"
 
 	"github.com/go-chi/chi"
 )
 
 func InitRouter(app *handler.Application) *chi.Mux {
 	r := chi.NewRouter()
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		response.NotFound(w, "requested url is unavailable")
+		return
+	})
+
+	r.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
+		response.NotAllowded(w, "method not allowed")
+		return
+	})
 
 	r.Post("/tags/{publication}", app.Store())
 	r.Get("/tags/{publication}", app.Get())
