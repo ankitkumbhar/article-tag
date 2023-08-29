d46f849e22238cf823707f6991c54d4c54f93dfa
ankitkumbhar <kumbharankit23@gmail.com>
2023-08-29 17:56:08 +0530

    added func comment and using route group

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -11,20 +11,25 @@ import (
 func InitRouter(app *handler.Application) *chi.Mux {
 	r := chi.NewRouter()
 
+	// sets a custom message for 404 error status code
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		response.NotFound(w, "requested url is unavailable")
 		return
 	})
 
+	// sets custom message for 405 error status code
 	r.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
 		response.NotAllowded(w, "method not allowed")
 		return
 	})
 
-	r.Post("/tags/{publication}", app.Store())
-	r.Get("/tags/{publication}", app.Get())
-	r.Delete("/tags/{publication}", app.Delete())
-	r.Get("/tags/{publication}/popular", app.PopularTag())
+	// route group
+	r.Route("/tags", func(r chi.Router) {
+		r.Post("/{publication}", app.Store())
+		r.Get("/{publication}", app.Get())
+		r.Delete("/{publication}", app.Delete())
+		r.Get("/{publication}/popular", app.PopularTag())
+	})
 
 	return r
 }
